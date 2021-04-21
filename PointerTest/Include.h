ac9283bff5e76eac7530012add4f650be91dc48b
jyyblue <jyyblue1987@outlook.com>
2021-04-21 17:22:17 +0800

    [add] add dummy body.

--- PointerTest/Include.h
+++ PointerTest/Include.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <memory>
 #include <ostream>
+#include <string>
 #include "LinkedBag.h"
 
 using namespace std;
--- PointerTest/LinkedBag340.cpp
+++ PointerTest/LinkedBag340.cpp
@@ -6,3 +6,66 @@
 ===>  PLEASE ADD CODE	<===
 ===>					<===
 */
+
+#include "Include.h"
+
+template<typename ItemType>
+bool LinkedBag<ItemType>::removeSecondNode340()
+{
+	return true;
+}
+
+template<typename ItemType>
+bool LinkedBag<ItemType>::addEnd340(const ItemType&)
+{
+	return true;
+}
+
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340Iterative() const
+{
+	return -1;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340Recursive() const
+{
+	return -1;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340RecursiveNoHelper() const
+{
+	return -1;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340Recursive(const ItemType&) const
+{
+	return -1;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340RecursiveNoHelper(const ItemType&) const
+{
+	return -1;
+}
+
+template<typename ItemType>
+ItemType LinkedBag<ItemType>::removeRandom340()
+{
+	return ItemType();
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340RecursiveHelper(Node<ItemType>*) const // if needed
+{
+	return -1;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340RecursiveHelper(Node<ItemType>*, const ItemType&) const // if needed
+{
+	return -1;
+}
